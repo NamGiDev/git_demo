e2a5327f4ddaf5e826160d30650804692a5be3f5
macDev <scandragnel@gmail.com>
2025-12-11 08:15:06 -0400

    feat: Implementar PosgreSql

--- Proyectos/django_blog/django_blog/settings.py
+++ Proyectos/django_blog/django_blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db.wsgzbrddgbuwyodzutdz.supabase.co',
+        'NAME': 'postgres',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': '+7MVh?y9_smmrc!'
     }
 }
 
@@ -132,3 +136,6 @@ CORS_ALLOWED_ORIGINS = [
 
 # Permitir cookies/credenciales (útil para autenticación en el futuro)
 CORS_ALLOW_CREDENTIALS = True
+
+# Configuración agregada para eliminar la advertencia W042
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
